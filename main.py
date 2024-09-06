b5d6aaa0a4c7008869ae8aabb3cdc5faa1721106
Jemima-Renisha <renishareni08@gmail.com>
2024-09-06 21:51:25 +0530

    datatypes

---
+++ main.py
@@ -0,0 +1,18 @@
+# datatypes
+# 1.string datatype
+x = "Jemima" #string
+age = "28" #string
+print(age)
+print(x)
+
+#2.integer datatype(numbers without points)
+age = 28
+print(age)
+
+#3. float datatype (numbers with points)
+weight = 50.5
+print(weight)
+
+#4. boolean datatype
+student = "Jemima"
+print(False)
